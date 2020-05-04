ead09b12b213020f343b3556bf6e2a759bec9180
emacgregor <35611646+emacgregor@users.noreply.github.com>
2020-05-03 20:00:06 -0400

    Edit data

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+from datetime import date
 from os import path
 from tkinter import *
 from tkinter import ttk
@@ -17,7 +18,7 @@ class Form(Frame):
         self.loadData()
 
         f1 = Frame(self)
-        f1.pack(fill = X)
+        f1.pack(fill = X, padx = 10, pady = 10)
 
         # Labels for each field
         LEFT_LABEL_WIDTH = 16
@@ -68,7 +69,8 @@ class Form(Frame):
         self.la.grid(row = 4, column = 1, padx = 2)
         self.fu = DateEntry(f1)
         self.fu.grid(row = 4, column = 2, padx = 2)
-        self.clinician = ttk.Combobox(f1, width = 12, values=CLINICIANS)
+        self.clinicianText = StringVar()
+        self.clinician = ttk.Combobox(f1, width = 12, text = self.clinicianText, values = CLINICIANS)
         self.clinician.grid(row = 5, column = 1, padx = 2)
 
         # Right column fields
@@ -93,7 +95,8 @@ class Form(Frame):
         self.rdvmLast.grid(row = 3, column = 5, sticky = "w")
         self.rdvmFirst = Text(f1, width = 12, height = 1)
         self.rdvmFirst.grid(row = 3, column = 5, sticky = "w", padx = (105, 0))
-        self.practice = ttk.Combobox(f1, width = 31, values = RDVMS)
+        self.practiceText = StringVar()
+        self.practice = ttk.Combobox(f1, width = 31, text = self.practiceText, values = RDVMS)
         self.practice.grid(row = 4, column = 5, sticky = "w")
         self.rdvmPhone1 = Text(f1, width = 12, height = 1)
         self.rdvmPhone1.grid(row = 5, column = 5, sticky = "w")
@@ -103,12 +106,13 @@ class Form(Frame):
         self.rdvmFAX.grid(row = 5, column = 5, sticky = "w", padx = (210, 0))
 
         # Submit button
-        Button(f1, text = "Submit", fg = "Black",
-            command = self.submit).grid(row = 6, column = 5)
+        self.submitButton = Button(f1, text = "Submit", fg = "Black",
+            command = self.submit)
+        self.submitButton.grid(row = 6, column = 5)
 
         # Frame 2 contains the information on pets
         f2 = Frame(self)
-        f2.pack()
+        f2.pack(padx = 10)
 
         # Listbox all the basic pet information
         self.listBox = ttk.Treeview(f2, columns = list(self.data.columns), show = 'headings')
@@ -117,8 +121,13 @@ class Form(Frame):
             self.listBox.heading(i, text = col)
             self.listBox.column(i, width = DEFAULT_COLUMN_WIDTH)
             i += 1
+        i = 0
         for item in self.data.values:
-            self.listBox.insert("", "end", values = list(item))
+            self.listBox.insert("", "end", text = str(i), values = list(item))
+            i += 1
+
+        # Get info on click
+        self.listBox.bind("<Double-1>", self.fillFields)
 
         # Scrollbars
         xsb = ttk.Scrollbar(f2,orient = HORIZONTAL, command = self.listBox.xview)
@@ -144,6 +153,8 @@ class Form(Frame):
                 'Sex': [],
                 'Birthdate': [],
                 'Deathdate': [],
+                'LAdate': [],
+                'FUdate': [],
                 'Client last': [],
                 'Client first': [],
                 'Clinician': [],
@@ -163,32 +174,36 @@ class Form(Frame):
 
     def submit(self):
         newData = {
-            'Case number': self.caseNum.get("1.0", END),
-            'Client number': self.clientNum.get("1.0", END),
-            'Patient': self.patient.get("1.0", END),
-            'Species': self.species.get("1.0", END),
-            'Breed': self.breed.get("1.0", END),
-            'Sex': self.sex.get("1.0", END),
+            'Case number': self.caseNum.get("1.0", "end-1c"),
+            'Client number': self.clientNum.get("1.0", "end-1c"),
+            'Patient': self.patient.get("1.0", "end-1c"),
+            'Species': self.species.get("1.0", "end-1c"),
+            'Breed': self.breed.get("1.0", "end-1c"),
+            'Sex': self.sex.get("1.0", "end-1c"),
             'Birthdate': self.birth.get(),
             'Deathdate': self.death.get(),
-            'Client last': self.clientFirst.get("1.0", END),
-            'Client first': self.clientLast.get("1.0", END),
+            'LAdate': self.la.get(),
+            'FUdate': self.fu.get(),
+            'Client last': self.clientLast.get("1.0", "end-1c"),
+            'Client first': self.clientFirst.get("1.0", "end-1c"),
             'Clinician': self.clinician.get(),
-            'City': self.city.get("1.0", END),
-            'State': self.state.get("1.0", END),
-            'ZIP': self.zipCode.get("1.0", END),
-            'Home phone': self.homePhone.get("1.0", END),
-            'Work phone': self.workPhone.get("1.0", END),
-            'Alt phone': self.altPhone.get("1.0", END),
-            'RDVM last': self.rdvmLast.get("1.0", END),
-            'RDVM first': self.rdvmFirst.get("1.0", END),
+            'City': self.city.get("1.0", "end-1c"),
+            'State': self.state.get("1.0", "end-1c"),
+            'ZIP': self.zipCode.get("1.0", "end-1c"),
+            'Home phone': self.homePhone.get("1.0", "end-1c"),
+            'Work phone': self.workPhone.get("1.0", "end-1c"),
+            'Alt phone': self.altPhone.get("1.0", "end-1c"),
+            'RDVM last': self.rdvmLast.get("1.0", "end-1c"),
+            'RDVM first': self.rdvmFirst.get("1.0", "end-1c"),
             'Practice': self.practice.get(),
-            'RDVM phone 1': self.rdvmPhone1.get("1.0", END),
-            'RDVM phone 2': self.rdvmPhone2.get("1.0", END),
-            'RDVM fax': self.rdvmFAX.get("1.0", END)}
+            'RDVM phone 1': self.rdvmPhone1.get("1.0", "end-1c"),
+            'RDVM phone 2': self.rdvmPhone2.get("1.0", "end-1c"),
+            'RDVM fax': self.rdvmFAX.get("1.0", "end-1c")}
 
         self.listBox.insert("", "end", values = list(newData.values()))
         self.data = self.data.append(newData, ignore_index = True)
+        self.clearFields()
+
         self.saveData()
 
     def saveData(self):
@@ -197,6 +212,107 @@ class Form(Frame):
             os.mkdir(DATA_DIR)
         # Save data
         self.data.to_pickle(DATA_PATH)
+
+    def clearFields(self):
+        self.setText(self.caseNum, "")
+        self.setText(self.clientNum, "")
+        self.setText(self.patient, "")
+        self.setText(self.species, "")
+        self.setText(self.breed, "")
+        self.setText(self.sex, "")
+        self.birth.set_date(date.today())
+        self.death.set_date(date.today())
+        self.la.set_date(date.today())
+        self.fu.set_date(date.today())
+        self.setText(self.clientLast, "")
+        self.setText(self.clientFirst, "")
+        self.clinicianText.set("")
+        self.setText(self.city, "")
+        self.setText(self.state, "")
+        self.setText(self.zipCode, "")
+        self.setText(self.homePhone, "")
+        self.setText(self.workPhone, "")
+        self.setText(self.altPhone, "")
+        self.setText(self.rdvmLast, "")
+        self.setText(self.rdvmFirst, "")
+        self.practiceText.set("")
+        self.setText(self.rdvmPhone1, "")
+        self.setText(self.rdvmPhone2, "")
+        self.setText(self.rdvmFAX, "")
+
+        self.submitButton.configure(text = "Submit", command = self.submit)
+
+    def fillFields(self, event):
+        item = self.listBox.selection()[0]
+        values = self.listBox.item(item, "values")
+
+        self.setText(self.caseNum, values[0])
+        self.setText(self.clientNum, values[1])
+        self.setText(self.patient, values[2])
+        self.setText(self.species, values[3])
+        self.setText(self.breed, values[4])
+        self.setText(self.sex, values[5])
+        self.birth.set_date(values[6])
+        self.death.set_date(values[7])
+        self.la.set_date(values[8])
+        self.fu.set_date(values[9])
+        self.setText(self.clientLast, values[10])
+        self.setText(self.clientFirst, values[11])
+        self.clinicianText.set(values[12])
+        self.setText(self.city, values[13])
+        self.setText(self.state, values[14])
+        self.setText(self.zipCode, values[15])
+        self.setText(self.homePhone, values[16])
+        self.setText(self.workPhone, values[17])
+        self.setText(self.altPhone, values[18])
+        self.setText(self.rdvmLast, values[19])
+        self.setText(self.rdvmFirst, values[20])
+        self.practiceText.set(values[21])
+        self.setText(self.rdvmPhone1, values[22])
+        self.setText(self.rdvmPhone2, values[23])
+        self.setText(self.rdvmFAX, values[24])
+
+        self.submitButton.configure(text = "Modify", command = lambda: self.modify(item))
+
+    def modify(self, item):
+        moddedData = {
+            'Case number': self.caseNum.get("1.0", "end-1c"),
+            'Client number': self.clientNum.get("1.0", "end-1c"),
+            'Patient': self.patient.get("1.0", "end-1c"),
+            'Species': self.species.get("1.0", "end-1c"),
+            'Breed': self.breed.get("1.0", "end-1c"),
+            'Sex': self.sex.get("1.0", "end-1c"),
+            'Birthdate': self.birth.get(),
+            'Deathdate': self.death.get(),
+            'LAdate': self.la.get(),
+            'FUdate': self.fu.get(),
+            'Client last': self.clientLast.get("1.0", "end-1c"),
+            'Client first': self.clientFirst.get("1.0", "end-1c"),
+            'Clinician': self.clinician.get(),
+            'City': self.city.get("1.0", "end-1c"),
+            'State': self.state.get("1.0", "end-1c"),
+            'ZIP': self.zipCode.get("1.0", "end-1c"),
+            'Home phone': self.homePhone.get("1.0", "end-1c"),
+            'Work phone': self.workPhone.get("1.0", "end-1c"),
+            'Alt phone': self.altPhone.get("1.0", "end-1c"),
+            'RDVM last': self.rdvmLast.get("1.0", "end-1c"),
+            'RDVM first': self.rdvmFirst.get("1.0", "end-1c"),
+            'Practice': self.practice.get(),
+            'RDVM phone 1': self.rdvmPhone1.get("1.0", "end-1c"),
+            'RDVM phone 2': self.rdvmPhone2.get("1.0", "end-1c"),
+            'RDVM fax': self.rdvmFAX.get("1.0", "end-1c")}
+        self.listBox.item(item, values = list(moddedData.values()))
+
+        row = int(item[-1]) - 1
+
+        self.data.loc[row] = list(moddedData.values())
+        self.clearFields()
+
+        self.saveData()
+
+    def setText(self, widget, value):
+        widget.delete(1.0, "end-1c")
+        widget.insert("end-1c", value)
 
 
 # Create a GUI window
